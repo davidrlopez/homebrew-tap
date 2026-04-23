9413b4e197fc4bda64fbed0b42f368523b98ea69
davidrlopez <screen.mp3@gmail.com>
2026-04-24 01:26:57 +0200

    Url adjusted

--- smb-automount.rb
+++ smb-automount.rb
@@ -1,6 +1,6 @@
 class SmbAutomount < Formula
   desc "macOS SMB Auto-Mount (Tailscale/VPN Optimized)"
-  homepage "https://github.com/davidromanlopez/smb-automount"
+  homepage "https://github.com/davidrlopez/smb-automount"
   url "https://github.com/davidrlopez/smb-automount/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
